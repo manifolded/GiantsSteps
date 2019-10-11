27011d8dd2b50543dd803eaf31bc247cd587b8a5
Keith Matthews <keith@alice.caltech.edu>
2019-10-10 21:31:36 -0700

    Functional basic test of triangular prism generation.

---
+++ BabySteps/Assets/Scripts/GeneratePrism.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GeneratePrism : MonoBehaviour
+{
+    public GameObject mesh2DHolder;
+    public Material mat;
+
+    void Start()
+    {
+        Mesh mesh2D = mesh2DHolder.GetComponent<MeshFilter>().mesh;
+
+        Vector3[] vertices2D = mesh2D.vertices;
+        int[] triangles2D = mesh2D.triangles;
+
+        // select triangle from 2D mesh
+        int triangle2DIndex = 0;
+        int[] triangle2D =
+            {triangles2D[triangle2DIndex*3 + 0],
+             triangles2D[triangle2DIndex*3 + 1],
+             triangles2D[triangle2DIndex*3 + 2]
+            };
+
+        List<int> triangles = new List<int> { 0, 1, 2 };
+        //List<Vector3> vertices = new List<Vector3>
+        //{
+        //    vertices2D[triangle2D[0]],
+        //    vertices2D[triangle2D[1]],
+        //    vertices2D[triangle2D[2]]
+        //};
+
+        List<Vector3> vertices = new List<Vector3> {};
+
+        for (int i = 0; i < 3; i++) {
+
+            Vector3 xyVertex = vertices2D[triangle2D[i]];
+
+            // perform coordinate rotation as vertices are added.
+            vertices.Add(new Vector3(xyVertex[0], -xyVertex[2], xyVertex[1]));
+
+        }
+
+        //Debug.Log(vertices[0]);
+        //Debug.Log(vertices[1]);
+        //Debug.Log(vertices[2]);
+
+        // add the back face
+        Vector3 depth = new Vector3(0, 20, 0);
+        vertices.Add(vertices[0] + depth);
+        vertices.Add(vertices[1] + depth);
+        vertices.Add(vertices[2] + depth);
+
+        //   note order intentionally reversed for outward pointing normal
+        triangles.Add(5);
+        triangles.Add(4);
+        triangles.Add(3);
+
+        // add side face for (0,1) edge
+        //    1st triangle (0, 3, 4)
+        triangles.Add(0);
+        triangles.Add(3);
+        triangles.Add(4);
+        //    2nd triangle (0, 4, 1)
+        triangles.Add(0);
+        triangles.Add(4);
+        triangles.Add(1);
+
+        // add side face for (1, 2) edge
+        //    1st triangle (1, 4, 5)
+        triangles.Add(1);
+        triangles.Add(4);
+        triangles.Add(5);
+        //    2nd triangle (1, 5, 2)
+        triangles.Add(1);
+        triangles.Add(5);
+        triangles.Add(2);
+
+        // add side face for (2, 0) edge
+        //    1st triangle (2, 5, 3)
+        triangles.Add(2);
+        triangles.Add(5);
+        triangles.Add(3);
+        //    2nd triangle (2, 3, 0)
+        triangles.Add(2);
+        triangles.Add(3);
+        triangles.Add(0);
+
+
+        // Assemble output game object
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        Mesh mesh = gameObject.GetComponent<MeshFilter>().mesh;
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        //mesh.uv = new Vector2[]
+        //    { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1) };
+
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+        //mesh.Optimize();
+
+
+        gameObject.GetComponent<MeshRenderer>().material = mat;
+
+    }
+
+}
---
+++ BabySteps/Assets/Scripts/UsageTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using mattatz.Utils;
+using mattatz.Triangulation2DSystem;
+
+public class UsageTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // input points for a polygon2D contor
+        List<Vector2> points = new List<Vector2>();
+
+        // Add Vector2 to points
+        points.Add(new Vector2(-2.5f, -2.5f));
+        points.Add(new Vector2(2.5f, -2.5f));
+        points.Add(new Vector2(4.5f, 2.5f));
+        points.Add(new Vector2(0.5f, 4.5f));
+        points.Add(new Vector2(-3.5f, 2.5f));
+
+        // construct Polygon2D 
+        Polygon2D polygon = Polygon2D.Contour(points.ToArray());
+
+        // construct Triangulation2D with Polygon2D and threshold angle (18f ~ 27f recommended)
+        Triangulation2D triangulation = new Triangulation2D(polygon, 22.5f);
+
+        // build a mesh from triangles in a Triangulation2D instance
+        Mesh mesh = triangulation.Build();
+
+        // Initialize gameObject
+        //   assuming it starts out as an empty object
+        gameObject.AddComponent<MeshFilter>();
+
+        // GetComponent<MeshFilter>().sharedMesh = mesh;
+        //gameObject.GetComponent<MeshFilter>().sharedMesh = mesh;
+        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>();
+
+        //gameObject.AddComponent<MeshCollider>();
+
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
