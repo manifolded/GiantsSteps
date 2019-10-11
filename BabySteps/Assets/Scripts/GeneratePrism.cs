ca4cf3ffaa1184511443224bba5d83ec2197e7ba
Keith Matthews <keith@alice.caltech.edu>
2019-10-10 22:37:00 -0700

    All new prism code.  Prism now has discontinuous normals and sharp edges.

--- BabySteps/Assets/Scripts/GeneratePrism.cs
+++ BabySteps/Assets/Scripts/GeneratePrism.cs
@@ -10,83 +10,20 @@ public class GeneratePrism : MonoBehaviour
 
     void Start()
     {
+        // select 2D mesh triangle to use as cross-section
+        int frontIndex2D = 2;
+        float depth = 20;
+
         Mesh mesh2D = mesh2DHolder.GetComponent<MeshFilter>().mesh;
 
         Vector3[] vertices2D = mesh2D.vertices;
         int[] triangles2D = mesh2D.triangles;
+        List<Vector3> vertices = new List<Vector3> { };
+        List<int> triangles = new List<int> { };
 
-        // select triangle from 2D mesh
-        int triangle2DIndex = 0;
-        int[] triangle2D =
-            {triangles2D[triangle2DIndex*3 + 0],
-             triangles2D[triangle2DIndex*3 + 1],
-             triangles2D[triangle2DIndex*3 + 2]
-            };
-
-        List<int> triangles = new List<int> { 0, 1, 2 };
-        //List<Vector3> vertices = new List<Vector3>
-        //{
-        //    vertices2D[triangle2D[0]],
-        //    vertices2D[triangle2D[1]],
-        //    vertices2D[triangle2D[2]]
-        //};
-
-        List<Vector3> vertices = new List<Vector3> {};
-
-        for (int i = 0; i < 3; i++) {
-
-            Vector3 xyVertex = vertices2D[triangle2D[i]];
-
-            // perform coordinate rotation as vertices are added.
-            vertices.Add(new Vector3(xyVertex[0], -xyVertex[2], xyVertex[1]));
-
-        }
-
-        //Debug.Log(vertices[0]);
-        //Debug.Log(vertices[1]);
-        //Debug.Log(vertices[2]);
-
-        // add the back face
-        Vector3 depth = new Vector3(0, 20, 0);
-        vertices.Add(vertices[0] + depth);
-        vertices.Add(vertices[1] + depth);
-        vertices.Add(vertices[2] + depth);
-
-        //   note order intentionally reversed for outward pointing normal
-        triangles.Add(5);
-        triangles.Add(4);
-        triangles.Add(3);
-
-        // add side face for (0,1) edge
-        //    1st triangle (0, 3, 4)
-        triangles.Add(0);
-        triangles.Add(3);
-        triangles.Add(4);
-        //    2nd triangle (0, 4, 1)
-        triangles.Add(0);
-        triangles.Add(4);
-        triangles.Add(1);
-
-        // add side face for (1, 2) edge
-        //    1st triangle (1, 4, 5)
-        triangles.Add(1);
-        triangles.Add(4);
-        triangles.Add(5);
-        //    2nd triangle (1, 5, 2)
-        triangles.Add(1);
-        triangles.Add(5);
-        triangles.Add(2);
-
-        // add side face for (2, 0) edge
-        //    1st triangle (2, 5, 3)
-        triangles.Add(2);
-        triangles.Add(5);
-        triangles.Add(3);
-        //    2nd triangle (2, 3, 0)
-        triangles.Add(2);
-        triangles.Add(3);
-        triangles.Add(0);
-
+        AddFrontFace(vertices, triangles, vertices2D, triangles2D, frontIndex2D);
+        AddBackFace(vertices, triangles, vertices2D, triangles2D, frontIndex2D, depth);
+        AddSideFaces(vertices, triangles, vertices2D, triangles2D, frontIndex2D, depth);
 
         // Assemble output game object
         gameObject.AddComponent<MeshFilter>();
@@ -101,11 +38,143 @@ public class GeneratePrism : MonoBehaviour
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
 
-        //mesh.Optimize();
-
-
         gameObject.GetComponent<MeshRenderer>().material = mat;
 
     }
 
+    // =========================================================================
+    void AddFrontFace(List<Vector3> vertices, List<int> triangles,
+                      Vector3[] vertices2D, int[] triangles2D, int frontIndex2D)
+    {
+        int numVerts = vertices.Count();
+
+        int[] triangle = {
+            triangles2D[frontIndex2D * 3 + 2],
+            triangles2D[frontIndex2D * 3 + 1],
+            triangles2D[frontIndex2D * 3 + 0]
+        };
+
+        for (int i = 0; i < 3; i++)
+        {
+            Vector3 vertex2D = vertices2D[triangle[i]];
+            // transform vertex2D to vertex3D and add
+            vertices.Add(new Vector3(vertex2D[0], -vertex2D[2], vertex2D[1]));
+
+            // add new triangle
+            triangles.Add(numVerts + i);
+        }
+
+    }
+
+    // =========================================================================
+    void AddBackFace(List<Vector3> vertices, List<int> triangles,
+                      Vector3[] vertices2D, int[] triangles2D, int frontIndex2D,
+                      float depth)
+    {
+        int numVerts = vertices.Count();
+
+        // reverse the orientation of the triangle
+        int[] triangle = {
+            triangles2D[frontIndex2D * 3 + 0],
+            triangles2D[frontIndex2D * 3 + 1],
+            triangles2D[frontIndex2D * 3 + 2]
+        };
+
+        for (int i = 0; i < 3; i++)
+        {
+            // pull out single vertex in 2D mesh coords
+            Vector3 vertex2D = vertices2D[triangle[i]];
+            // transform vertex2D to vertex3D in full 3D coords
+            Vector3 vertex3D = new Vector3(vertex2D[0], -vertex2D[2], vertex2D[1]);
+            // translate back face through depth
+            vertex3D += new Vector3(0, depth, 0);
+            // add
+            vertices.Add(vertex3D);
+
+            // add new triangle
+            triangles.Add(numVerts + i);
+        }
+
+    }
+
+    // =========================================================================
+    void AddSideFaceFromEdge(List<Vector3> vertices, List<int> triangles,
+                     Vector3[] vertices2D, int[] triangles2D,
+                     int[] edge, float depth)
+    {
+        int numVerts = vertices.Count();
+
+        int ea = edge[0];
+        int eb = edge[1];
+
+        // add the 'a' vertex
+        // index: numVerts + 0
+        Vector3 vertex2D = vertices2D[ea];
+        // transform vertex2D to vertex3D in full 3D coords
+        Vector3 vertex3D = new Vector3(vertex2D[0], -vertex2D[2], vertex2D[1]);
+        // add
+        vertices.Add(vertex3D);
+        int a = numVerts + 0;
+
+        // add the 'b' vertex
+        // index: numVerts + 1
+        vertex2D = vertices2D[eb];
+        vertex3D = new Vector3(vertex2D[0], -vertex2D[2], vertex2D[1]);
+        vertices.Add(vertex3D);
+        int b = numVerts + 1;
+
+        // add the a-prime vertex | 'a' translated to back face
+        // index: numVerts + 2
+        vertex2D = vertices2D[ea];
+        vertex3D = new Vector3(vertex2D[0], -vertex2D[2], vertex2D[1]);
+        vertex3D += new Vector3(0, depth, 0);
+        vertices.Add(vertex3D);
+        int ap = numVerts + 2;
+
+        // add the b-prime vertex | 'b' translated to back face
+        // index: numVerts + 3
+        vertex2D = vertices2D[eb];
+        vertex3D = new Vector3(vertex2D[0], -vertex2D[2], vertex2D[1]);
+        vertex3D += new Vector3(0, depth, 0);
+        vertices.Add(vertex3D);
+        int bp = numVerts + 3;
+
+        // add first triangle:  (a, b', a')
+        triangles.Add(a);
+        triangles.Add(bp);
+        triangles.Add(ap);
+
+        // add second triangle:  (a, b, b')
+        triangles.Add(a);
+        triangles.Add(b);
+        triangles.Add(bp);
+    }
+
+    // =========================================================================
+    void AddSideFaces(List<Vector3> vertices, List<int> triangles,
+                      Vector3[] vertices2D, int[] triangles2D,
+                      int frontIndex2D, float depth)
+    {
+        int numVerts = vertices.Count();
+
+        int[] triangle =
+        {
+            triangles2D[frontIndex2D*3 + 0],
+            triangles2D[frontIndex2D*3 + 1],
+            triangles2D[frontIndex2D*3 + 2]
+        };
+
+        // add all 3 side faces
+        for (int i = 0; i < 3; i++) {
+            int[] edge = { triangle[i], triangle[(i + 1)%3] };
+
+            AddSideFaceFromEdge(vertices, triangles, vertices2D, triangles2D,
+                                edge, depth);
+        }
+
+    }
 }
+
+
+
+
