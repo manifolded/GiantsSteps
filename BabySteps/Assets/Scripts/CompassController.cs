afa0e4f69a3913615add2ddd9dcbd5f1475de6bf
Keith Matthews <keith@alice.caltech.edu>
2019-10-16 20:39:19 -0700

    Compass is now aimable with ',' and '.' keys.

--- BabySteps/Assets/Scripts/CompassController.cs
+++ BabySteps/Assets/Scripts/CompassController.cs
@@ -6,6 +6,9 @@ public class CompassController : MonoBehaviour
 {
     private Camera cam;
 
+    public float aim = 0;
+    public float aimSpeed = 25;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,10 @@ public class CompassController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float aimAxis = Input.GetAxis("Aim");
+        aim += aimAxis * Time.deltaTime * aimSpeed;
+
         float phi = cam.transform.eulerAngles.y;
-        transform.localRotation = Quaternion.Euler(new Vector3(90, -phi, 0));
+        transform.localRotation = Quaternion.Euler(new Vector3(90, aim - phi, 0));
     }
 }
