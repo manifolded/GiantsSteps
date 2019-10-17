eeac2ed82795f2d777a0e5a4c279324744a472b1
Keith Matthews <keith@alice.caltech.edu>
2019-10-16 20:13:32 -0700

    Added basic compass in player's view.

---
+++ BabySteps/Assets/Scripts/CompassController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassController : MonoBehaviour
+{
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float phi = cam.transform.eulerAngles.y;
+        transform.localRotation = Quaternion.Euler(new Vector3(90, -phi, 0));
+    }
+}
